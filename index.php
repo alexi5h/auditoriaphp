1ce90c62427034458dc8d1fb647f54726549d408
Alexi5h <alexi5h@hotmail.com>
2015-04-14 23:38:37 -0500

    1st commit
    Signed-off-by: Diego <diegoq@gmail.com>

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--><html lang="en"> <!--<![endif]-->
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Auditoría PHP</title>
+    </head>
+    
+    <body>
+        <h2>Bienvenido...</h2>
+    </body>
