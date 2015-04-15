719ea3cfeb708c958fd630320374b1c351801705
Alexi5h <alexi5h@hotmail.com>
2015-04-14 23:38:37 -0500

    1st commit

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Auditoría</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/font-awesome.min.css" />-->
+        <link rel="stylesheet" type="text/css" href="assets/plugins/loginbatmanfiles/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="assets/plugins/loginbatmanfiles/css/style.css" />
+
+    </head>
+    <body id="login">
+        <img id="bg" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/Download-Free-Website-Abstract-Wallpaper.jpg" alt="Fondo" />
+        <?php echo $content ?>
+        <!--<script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>-->
+        <script src="assets/plugins/loginbatmanfiles/js/prefixfree.min.js"></script>
+        <script src="assets/plugins/loginbatmanfiles/js/modernizr.js"></script>
+        <span class="author">Designed by Alexis Hidalgo</span>
+    </body>
+</html>
